2a18089cd972966b1759e28570b86f108001e976
berkan sems <58864216+berkansems@users.noreply.github.com>
2021-03-19 09:46:52 +0300

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -164,5 +164,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'lizartinsaat@gmail.com'
-EMAIL_HOST_PASSWORD = '123456+test'
+EMAIL_HOST_PASSWORD = 'RR'
 
